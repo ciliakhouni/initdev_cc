786e3b884147580677e6e0efa0377efd9987a70f
ciliakhouni <ciliaxouni@gmail.com>
2018-04-05 01:53:51 +0000

    fait

--- main.sh
+++ main.sh
@@ -10,3 +10,83 @@ echo $0
 echo "uknown arguments , please check the help: initdev -help"
 echo $1
 fi 
+
+if [ $test " language" = "c" ] && [ $test "LICENSE"="MIT"]
+then $( cp -r initdev_cc/source/lisenses/MIT  initdev_cc/nomprojet/LICENSE) && ( $ cp -r source/main.c nomprojet/main.c)
+
+echo $0
+ else 
+ 
+echo "uknown arguments , please check the help: initdev -help"
+echo $1
+fi 
+
+if [ $test " language" = "CPP" ] && [ $test "LICENSE"="GPL"]
+then $( cp -r initdev_cc/source/lisenses/GPL  initdev_cc/nomprojet/LICENSE) && $ ( cp -r source/main.cPP nomprojet/main.cPP)
+
+echo $0
+ else 
+ 
+echo "uknown arguments , please check the help: initdev -help"
+echo $1
+fi 
+
+if [ $test " language" = "py" ] && [ $test "LICENSE"="GPL"];
+then $( cp -r initdev_cc/source/lisenses/GPL  initdev_cc/nomprojet/LICENSE) && $ ( cp -r source/main.py nomprojet/main.py)
+
+echo $0
+ else 
+ 
+echo "uknown arguments , please check the help: initdev -help"
+echo $1
+fi 
+
+if [ $test " language" = "LATEX" ] && [ $test "LICENSE"="GPL"]
+then $( cp -r initdev_cc/source/lisenses/GPL  initdev_cc/nomprojet/LICENSE) && ( $ cp -r source/main.tex nomprojet/main.tex)
+
+echo $0
+ else 
+ 
+echo "uknown arguments , please check the help: initdev -help"
+echo $1
+fi 
+
+if [ $test " language" = "BEAMER" ] && [ $test "LICENSE"="GPL"]
+then $( cp -r initdev_cc/source/lisenses/GPL  initdev_cc/nomprojet/LICENSE) && ( $ cp -r source/main.BEAMIR nomprojet/main.BEAMIR)
+
+echo $0
+ else 
+ 
+echo "uknown arguments , please check the help: initdev -help"
+echo $1
+fi 
+
+if [ $test " language" = "CPP" ] && [ $test "LICENSE"="MIT"]
+then $( cp -r initdev_cc/source/lisenses/MIT  initdev_cc/nomprojet/LICENSE) && ( $ cp -r source/main.CPP nomprojet/main.CPP)
+
+echo $0
+ else 
+ 
+echo "uknown arguments , please check the help: initdev -help"
+echo $1
+fi 
+
+if [ $test " language" = "LATEX" ] && [ $test "LICENSE"="MIT"]
+then $( cp -r initdev_cc/source/lisenses/MIT  initdev_cc/nomprojet/LICENSE) && ( $ cp -r source/main.TEX nomprojet/main.TEX)
+
+echo $0
+ else 
+ 
+echo "uknown arguments , please check the help: initdev -help"
+echo $1
+fi 
+
+if [ $test " language" = "BEAMER" ] && [ $test "LICENSE"="MIT"]
+then $( cp -r initdev_cc/source/lisenses/MIT  initdev_cc/nomprojet/LICENSE) && ( $ cp -r source/main.BEAMER nomprojet/main.BEAMIR)
+
+echo $0
+ else 
+ 
+echo "uknown arguments , please check the help: initdev -help"
+echo $1
+fi 
