920354bac9863697d3ee582f186f986ec443b13d
ciliakhouni <ciliaxouni@gmail.com>
2018-04-05 01:16:36 +0000

    fait

--- main.sh
+++ main.sh
@@ -1,6 +1,12 @@
-(mkdir " projeto.p")
-( cd" projeto.p"
+(mkdir " nomprojet")
+( cd" nomprojet")
 (touch" LICENSE")
-if [ $test  language = "c" ];
-then  cp -r initdev_cc/lisenses/GPL initdev_cc/projeto.p/LICENSE 
+if [ $test " language" = "c" ] && [ $test "LICENSE"="GPL"]
+then $( cp -r initdev_cc/source/lisenses/GPL  initdev_cc/nomprojet/LICENSE) && ( $ cp -r source/main.c nomprojet/main.c)
 
+echo $0
+ else 
+ 
+echo "uknown arguments , please check the help: initdev -help"
+echo $1
+fi 
