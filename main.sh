7a576803f7f4392b75abc36ba105e867990f46ce
ciliaxouni <ciliaxouni@gmail.com>
2018-04-05 19:25:36 +0000

    fait

--- main.sh
+++ main.sh
@@ -2,7 +2,7 @@
 ( cd" nomprojet")
 (touch" LICENSE")
 if [ $test " language" = "c" ] && [ $test "LICENSE"="GPL"]
-then $( cp -r initdev_cc/source/lisenses/GPL  initdev_cc/nomprojet/LICENSE) && ( $ cp -r source/main.c nomprojet/main.c)
+then $( cp -r initdev_cc/source/licenses/GPL  initdev_cc/nomprojet/LICENSE) && ( $ cp -r source/main.c nomprojet/main.c)
 
 echo $0
  else 
@@ -12,7 +12,7 @@ echo $1
 fi 
 
 if [ $test " language" = "c" ] && [ $test "LICENSE"="MIT"]
-then $( cp -r initdev_cc/source/lisenses/MIT  initdev_cc/nomprojet/LICENSE) && ( $ cp -r source/main.c nomprojet/main.c)
+then $( cp -r initdev_cc/source/licenses/MIT  initdev_cc/nomprojet/LICENSE) && ( $ cp -r source/main.c nomprojet/main.c)
 
 echo $0
  else 
@@ -22,7 +22,7 @@ echo $1
 fi 
 
 if [ $test " language" = "CPP" ] && [ $test "LICENSE"="GPL"]
-then $( cp -r initdev_cc/source/lisenses/GPL  initdev_cc/nomprojet/LICENSE) && $ ( cp -r source/main.cPP nomprojet/main.cPP)
+then $( cp -r initdev_cc/source/licenses/GPL  initdev_cc/nomprojet/LICENSE) && $ ( cp -r source/main.cPP nomprojet/main.cPP)
 
 echo $0
  else 
@@ -32,7 +32,7 @@ echo $1
 fi 
 
 if [ $test " language" = "py" ] && [ $test "LICENSE"="GPL"];
-then $( cp -r initdev_cc/source/lisenses/GPL  initdev_cc/nomprojet/LICENSE) && $ ( cp -r source/main.py nomprojet/main.py)
+then $( cp -r initdev_cc/source/licenses/GPL  initdev_cc/nomprojet/LICENSE) && $ ( cp -r source/main.py nomprojet/main.py)
 
 echo $0
  else 
@@ -42,7 +42,7 @@ echo $1
 fi 
 
 if [ $test " language" = "LATEX" ] && [ $test "LICENSE"="GPL"]
-then $( cp -r initdev_cc/source/lisenses/GPL  initdev_cc/nomprojet/LICENSE) && ( $ cp -r source/main.tex nomprojet/main.tex)
+then $( cp -r initdev_cc/source/licenses/GPL  initdev_cc/nomprojet/LICENSE) && ( $ cp -r source/main.tex nomprojet/main.tex)
 
 echo $0
  else 
@@ -52,7 +52,7 @@ echo $1
 fi 
 
 if [ $test " language" = "BEAMER" ] && [ $test "LICENSE"="GPL"]
-then $( cp -r initdev_cc/source/lisenses/GPL  initdev_cc/nomprojet/LICENSE) && ( $ cp -r source/main.BEAMIR nomprojet/main.BEAMIR)
+then $( cp -r initdev_cc/source/licenses/GPL  initdev_cc/nomprojet/LICENSE) && ( $ cp -r source/main.tex  nomprojet/main.tex)
 
 echo $0
  else 
@@ -62,7 +62,7 @@ echo $1
 fi 
 
 if [ $test " language" = "CPP" ] && [ $test "LICENSE"="MIT"]
-then $( cp -r initdev_cc/source/lisenses/MIT  initdev_cc/nomprojet/LICENSE) && ( $ cp -r source/main.CPP nomprojet/main.CPP)
+then $( cp -r initdev_cc/source/licenses/MIT  initdev_cc/nomprojet/LICENSE) && ( $ cp -r source/main.CPP nomprojet/main.CPP)
 
 echo $0
  else 
@@ -72,7 +72,7 @@ echo $1
 fi 
 
 if [ $test " language" = "LATEX" ] && [ $test "LICENSE"="MIT"]
-then $( cp -r initdev_cc/source/lisenses/MIT  initdev_cc/nomprojet/LICENSE) && ( $ cp -r source/main.TEX nomprojet/main.TEX)
+then $( cp -r initdev_cc/source/licenses/MIT  initdev_cc/nomprojet/LICENSE) && ( $ cp -r source/main.TEX nomprojet/main.TEX)
 
 echo $0
  else 
@@ -82,7 +82,7 @@ echo $1
 fi 
 
 if [ $test " language" = "BEAMER" ] && [ $test "LICENSE"="MIT"]
-then $( cp -r initdev_cc/source/lisenses/MIT  initdev_cc/nomprojet/LICENSE) && ( $ cp -r source/main.BEAMER nomprojet/main.BEAMIR)
+then $( cp -r initdev_cc/source/licenses/MIT  initdev_cc/nomprojet/LICENSE) && ( $ cp -r source/main.tex nomprojet/main.tex)
 
 echo $0
  else 
@@ -90,3 +90,49 @@ echo $0
 echo "uknown arguments , please check the help: initdev -help"
 echo $1
 fi 
+mkdir" Makefile " 
+if [ $test " language" = "C" ] 
+then $( cp -r initdev_cc/source/makefile  initdev_cc/nomprojet/Makefile) && ( $ cp -r source/main.c nomprojet/main.c)
+
+echo $0
+ else 
+ 
+echo "uknown arguments , please check the help: initdev -help"
+echo $1
+if [ $test " language" = "py" ] 
+then $( cp -r initdev_cc/source/makefile  initdev_cc/nomprojet/Makefile) && ( $ cp -r source/main.py nomprojet/main.py)
+
+echo $0
+ else 
+ 
+echo "uknown arguments , please check the help: initdev -help"
+echo $1
+if [ $test " language" = "Latex" ] 
+then $( cp -r initdev_cc/source/makefile  initdev_cc/nomprojet/Makefile) && ( $ cp -r source/main.tex nomprojet/main.tex)
+
+echo $0
+ else 
+ 
+echo "uknown arguments , please check the help: initdev -help"
+echo $1
+if [ $test " language" = "CPP" ] 
+then $( cp -r initdev_cc/source/makefile  initdev_cc/nomprojet/Makefile) && ( $ cp -r source/main.cpp nomprojet/main.cpp)
+
+echo $0
+ else 
+ 
+echo "uknown arguments , please check the help: initdev -help"
+echo $1
+if [ $test " language" = "BEAMER" ] 
+then $( cp -r initdev_cc/source/makefile  initdev_cc/nomprojet/Makefile) && ( $ cp -r source/main.tex nomprojet/main.tex)
+
+echo $0
+ else 
+ 
+echo "uknown arguments , please check the help: initdev -help"
+echo $1
+
+
+
+
+
